ff5d8cd16c1d3ce111d6124bbc9da20a9ba54aad
AjayKondath <ajaykondath@gmail.com>
2016-03-07 22:09:07 -0500

    Merge

--- src/main/java/com/quicktap/Utils.java
+++ src/main/java/com/quicktap/Utils.java
@@ -6,6 +6,10 @@ package com.quicktap;
 import java.time.Instant;
 import java.util.Date;
 
+import org.neo4j.cypher.internal.frontend.v2_3.perty.recipe.DocRecipe.strategyExpander;
+
+import lombok.NonNull;
+
 /**
  * @author Aashish
  *
@@ -18,5 +22,11 @@ public class Utils {
 	public static Date getTime() {
 		return Date.from(Instant.now());
 	}
+	public static int getIdByQuestion(@NonNull String string) {
+		return Integer.valueOf(string.trim().substring(0, 2).trim());
+	}
 	
+	public static String getOnlyTitle(@NonNull String title) {
+		return title.substring(title.indexOf("-")+1,title.length());
+	}
 }
