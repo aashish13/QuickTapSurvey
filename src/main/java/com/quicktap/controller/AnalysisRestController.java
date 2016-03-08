d28b0f24f1a17c05f565b75b1cd23df47ff6ad15
AjayKondath <ajaykondath@gmail.com>
2016-03-07 22:09:39 -0500

    Merge

---
+++ src/main/java/com/quicktap/controller/AnalysisRestController.java
@@ -0,0 +1,30 @@
+/**
+ * 
+ */
+package com.quicktap.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.quicktap.model.ChartData;
+import com.quicktap.service.VisualizationService;
+
+/**
+ * @author Aashish
+ *
+ */
+
+@RestController
+public class AnalysisRestController {
+	@Autowired
+	private VisualizationService visualizationService;
+	
+	@RequestMapping("getvisualizationdata/{chartType}/{questionId}")
+	public ChartData show(@PathVariable int questionId,@PathVariable String chartType){
+		ChartData chartData=visualizationService.getChartData(questionId, chartType);
+		return chartData;
+	}
+}
