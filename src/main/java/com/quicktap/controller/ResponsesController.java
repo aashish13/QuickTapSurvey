d315f86ec1e625bd8f28c3f4902ab6cd978676c1
AjayKondath <ajaykondath@gmail.com>
2016-03-07 20:19:06 -0500

    merged

--- src/main/java/com/quicktap/controller/ResponsesController.java
+++ src/main/java/com/quicktap/controller/ResponsesController.java
@@ -3,10 +3,20 @@
  */
 package com.quicktap.controller;
 
+
+import java.util.Set;
+
+import javax.servlet.http.HttpServletRequest;
+
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
+
+
+import com.quicktap.data.entity.Questions;
+import com.quicktap.data.entity.Responses;
 
 import com.quicktap.data.entity.Surveys;
 import com.quicktap.service.SurveyService;
@@ -21,13 +31,22 @@ public class ResponsesController {
 	private SurveyService surveyService;
 	
 	@RequestMapping(value="/response")
-	public ModelAndView showResponsesPage(){
+
+	public ModelAndView showResponsesPage(HttpServletRequest request){
+		//for a survey id, get the questions and responses. 
+		int id=Integer.parseInt(request.getParameter("id"));
 		//example for Ashwini
-		String username="";
-		int surveyId=0;
-		Surveys survey=surveyService.getBySurveyId(surveyId);
-		survey.getQuestionses();
-		//Ends
-		return new ModelAndView("response");
+
+		Surveys survey=surveyService.getById(id);
+			
+		Set<Questions> questions =survey.getQuestionses(); 
+		Set<Responses> responses = survey.getResponseses();
+		
+		ModelAndView responseMv = new ModelAndView("response");
+		responseMv.addObject("responses", responses);
+		responseMv.addObject("questions", questions);
+		return responseMv;
+		
+
 	}
 }
