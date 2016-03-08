d87aa17e7984b221b578461e7b7793b47b6343b5
AjayKondath <ajaykondath@gmail.com>
2016-03-07 22:18:27 -0500

    merged

---
+++ src/main/java/com/quicktap/data/dao/ChartsDao.java
@@ -0,0 +1,34 @@
+/**
+ * 
+ */
+package com.quicktap.data.dao;
+
+import java.util.List;
+import java.util.Set;
+
+import org.hibernate.Criteria;
+import org.hibernate.SessionFactory;
+import org.hibernate.criterion.Restrictions;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.quicktap.data.entity.Charts;
+
+/**
+ * @author Aashish
+ *
+ */
+@Transactional
+@Repository
+public class ChartsDao {
+	@Autowired
+    private SessionFactory sessionFactory;
+
+	public Charts getChartByName(String chartName) {
+		Criteria chartsCriteria=sessionFactory.getCurrentSession().createCriteria(Charts.class);
+		List<Charts> charts=(List<Charts>) chartsCriteria.add(Restrictions.eq("name", chartName)).list();
+		return charts.isEmpty()?null:charts.get(0);
+	}
+
+}
