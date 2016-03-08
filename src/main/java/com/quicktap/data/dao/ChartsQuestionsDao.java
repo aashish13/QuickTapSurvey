31279806890abba1da95e8b0dfd09d4aed953661
AjayKondath <ajaykondath@gmail.com>
2016-03-07 22:18:58 -0500

    merge

---
+++ src/main/java/com/quicktap/data/dao/ChartsQuestionsDao.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package com.quicktap.data.dao;
+
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.quicktap.data.entity.ChartsQuestions;
+
+/**
+ * @author Aashish
+ *
+ */
+@Transactional
+@Repository
+public class ChartsQuestionsDao{
+
+}
