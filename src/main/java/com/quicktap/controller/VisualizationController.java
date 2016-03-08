a843dab93bcb0482f21078dca876a04fd6098968
Aashish <aashishkumar.singh@hotmail.com>
2016-03-07 19:49:49 -0500

    Added visualization related code

--- src/main/java/com/quicktap/controller/VisualizationController.java
+++ src/main/java/com/quicktap/controller/VisualizationController.java
@@ -3,8 +3,15 @@
  */
 package com.quicktap.controller;
 
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.quicktap.service.QuestionService;
 
 /**
  * @author Aashish
@@ -12,9 +19,15 @@ import org.springframework.web.bind.annotation.RequestMapping;
  */
 @Controller
 public class VisualizationController {
-
-	@RequestMapping(value="charts")
-	public String showCharts() {
-		return "charts";
+	@Autowired
+	private QuestionService questionService;
+	@RequestMapping(value="visualize/{chart}/{surveyId}")
+	public ModelAndView showCharts(@PathVariable int surveyId,@PathVariable String chart) {
+		Map<Integer,String> questions=questionService.getQuestionsForVisualizationDropDown(chart,surveyId);
+		ModelAndView mv=new ModelAndView("visualize");
+		mv.addObject("questions",questions);
+		mv.addObject("chartType", chart);
+		//mv.addObject("data",data);
+		return mv;
 	}
 }
