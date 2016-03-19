49ca5af80519d7699bae793fe02c4ee8ac38b85c
Aashish <aashishkumar.singh@hotmail.com>
2016-03-19 17:51:49 -0400

    Resolved response page issue

--- src/main/java/com/quicktap/data/entity/Responses.java
+++ src/main/java/com/quicktap/data/entity/Responses.java
@@ -14,6 +14,7 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
+import javax.persistence.OrderBy;
 import javax.persistence.Table;
 
 /**
@@ -120,6 +121,7 @@ public class Responses implements java.io.Serializable {
 	}
 
 	@OneToMany(fetch = FetchType.EAGER, mappedBy = "responses")
+	@OrderBy("id")
 	public Set<ResponseValues> getResponseValueses() {
 		return this.responseValueses;
 	}
